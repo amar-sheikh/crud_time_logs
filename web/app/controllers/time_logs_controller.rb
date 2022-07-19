d8968643576aa9653d9321d971a82199e758eda1
umar <amar.m.sheikh@gmail.com>
2022-07-19 20:13:59 +0500

    make it possible to CRUD time logs according to specs, with tests

--- web/app/controllers/time_logs_controller.rb
+++ web/app/controllers/time_logs_controller.rb
@@ -1,5 +1,34 @@
 class TimeLogsController < ApplicationController
   def index
     @time_logs = TimeLog.all
-  end 
+  end
+  def new
+    @time_log = TimeLog.new
+  end
+  def create
+    @time_log = TimeLog.new(time_log_params)
+    if @time_log.save
+      redirect_to time_logs_path
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+    @time_log = TimeLog.find params[:id]
+  end
+
+  def update
+    @time_log = TimeLog.find params[:id]
+    if @time_log.update(time_log_params)
+      redirect_to time_logs_path
+    else
+      render action: :edit
+    end
+  end
+
+  private
+  def time_log_params
+    params.require(:time_log).permit(:description, :duration_hours)
+  end
 end
--- web/app/models/time_log.rb
+++ web/app/models/time_log.rb
@@ -3,6 +3,7 @@
 class TimeLog < ApplicationRecord
   STATUS_PENDING = 'pending'
   STATUS_BILLED = 'billed'
-  
+
   attribute :status, default: STATUS_PENDING
+  validates :duration_hours, numericality: { greater_than: 0 }
 end
